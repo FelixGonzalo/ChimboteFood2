60b5381432451daee0735f39f6a9073d83272a16
Gonzalo Castro Cubas <felixgonzalocastro@gmail.com>
2021-08-24 18:51:38 -0500

    Se agrego el Splash Screen

---
+++ app/src/main/java/com/example/chimbotefood2/SplashScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.example.chimbotefood2
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startActivity(Intent(this, MainActivity::class.java))
+    }
+}
